1fe890bcd1ec03ff0b5cf6dad34f7be066615ebb
Sebastian Bastidas <sbastidasr@gmail.com>
2017-12-02 12:38:50 -0500

    Moved public files

--- server.js
+++ server.js
@@ -24,54 +24,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/api/dir/', function(req, res, next) {
-  res.send({
-    path: path.join(__dirname, '/public/authpages/'),
-    path2: path.join(__dirname, '/public/authpages'),
-    path3: path.join(__dirname, 'public/authpages/'),
-    path4: path.join(__dirname, './public/authpages/'),
-    path5: path.join(__dirname, '../public/authpages/'),
-    path6: path.resolve(__dirname, '/public/authpages/'),
-    path7: path.resolve(__dirname, '/public/authpages'),
-    path8: path.resolve(__dirname, 'public/authpages/'),
-    path9: path.resolve(__dirname, './public/authpages/'),
-    path0: path.resolve(__dirname, '../public/authpages/'),
-    dirname: __dirname
-  });
-});
+
+app.use(express.static('public'))
+
+
+// app.get('/api/dir/', function(req, res, next) {
+//   res.send({
+//     path: path.join(__dirname, '/public/authpages/'),
+//     path2: path.join(__dirname, '/public/authpages'),
+//     path3: path.join(__dirname, 'public/authpages/'),
+//     path4: path.join(__dirname, './public/authpages/'),
+//     path5: path.join(__dirname, '../public/authpages/'),
+//     path6: path.resolve(__dirname, '/public/authpages/'),
+//     path7: path.resolve(__dirname, '/public/authpages'),
+//     path8: path.resolve(__dirname, 'public/authpages/'),
+//     path9: path.resolve(__dirname, './public/authpages/'),
+//     path0: path.resolve(__dirname, '../public/authpages/'),
+//     dirname: __dirname
+//   });
+// });
 
 //Authentication middleware provided by express-jwt. If you're not logged in, it says you're unauthorized.
 //Otherwise it allows you to access the resource
-app.use(ejwt({
-  secret: JWT_SECRET_KEY,
-  getToken: function fromHeader (req) {
-    return req.cookies['woofle-token'] || req.headers['woofle-token'];
-  },
-}).unless({path: ['/auth/login', '/auth/signup', 'public/authpages/loginsignup.css', 'public/authpages/loginsignup.js', '/']}));
+// app.use(ejwt({
+//   secret: JWT_SECRET_KEY,
+//   getToken: function fromHeader (req) {
+//     return req.cookies['woofle-token'] || req.headers['woofle-token'];
+//   },
+// }).unless({path: ['/auth/login', '/auth/signup', 'public/authpages/loginsignup.css', 'public/authpages/loginsignup.js', '/']}));
 
 // app.use('/search', function(req, res, next) {
 //   res.sendFile(path.resolve(path.join(__dirname, './public/searchpage/search.html'));
 // });
-
-app.get('/search', function(req, res, next) {
-  res.sendFile(path.resolve(__dirname, 'public/searchpage/search.html'));
-});
-
-app.use(express.static(path.join(__dirname, 'public/authpages'), {
-  extensions: ['html']
-}));
-
-app.use(express.static(path.join(__dirname, 'public/breedpage'), {
-  extensions: ['html']
-}));
-
-app.use(express.static(path.join(__dirname, 'public/favoritespage'), {
-  extensions: ['html']
-}));
-
-app.use(express.static(path.join(__dirname, 'public/searchpage'), {
-  extensions: ['html']
-}));
+// 
+// app.get('/search', function(req, res, next) {
+//   res.sendFile(path.resolve(__dirname, 'public/searchpage/search.html'));
+// });
+// 
+// app.use(express.static(path.join(__dirname, 'public/authpages'), {
+//   extensions: ['html']
+// }));
+// 
+// app.use(express.static(path.join(__dirname, 'public/breedpage'), {
+//   extensions: ['html']
+// }));
+// 
+// app.use(express.static(path.join(__dirname, 'public/favoritespage'), {
+//   extensions: ['html']
+// }));
+// 
+// app.use(express.static(path.join(__dirname, 'public/searchpage'), {
+//   extensions: ['html']
+// }));
 
 app.use('/auth', authRoutes);
 app.use('/breeds', breedRoutes);
@@ -79,23 +83,26 @@ app.use('/api/favorites', favoritesRoutes);
 
 
 
-app.use('/', function(req, res, next) {
-  // So when someone goes to root it redirects to login
-  res.sendFile(path.resolve(path.join(__dirname, 'public/authpages/login.html')));
-});
+// app.use('/', function(req, res, next) {
+//   // So when someone goes to root it redirects to login
+//   res.sendFile(path.resolve(path.join(__dirname, 'public/authpages/login.html')));
+// });
 
-// If we're about to 401, redirect to the login page
-app.use(function(err, req, res, next) {
-  if(401 == err.status) {
-    console.log(err);
-    return res.redirect('/auth/login');
-  }
-  if (11000 == err.code) {
-    console.log(err);
-    return res.redirect('/auth/login');
-  }
-  return res.status(500).json({message: 'Internal server error'});
-});
+
+
+// TODO: ADD 404 route
+// // If we're about to 401, redirect to the login page
+// app.use(function(err, req, res, next) {
+//   if(401 == err.status) {
+//     console.log(err);
+//     return res.redirect('/auth/login');
+//   }
+//   if (11000 == err.code) {
+//     console.log(err);
+//     return res.redirect('/auth/login');
+//   }
+//   return res.status(500).json({message: 'Internal server error'});
+// });
 
 let server;
 
