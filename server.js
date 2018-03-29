0f83638729c716d89d099f3f9fef9c9e5dd9e78e
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-03-28 21:19:31 -0700

    removed comments from server.js. altered formatting on config.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -17,18 +16,15 @@ const authRoutes = require('./server/auth/auth.route');
 const breedRoutes = require('./server/breed/breed.route');
 const favoritesRoutes = require('./server/favorites/favorites.route');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// UNPROTECTED ROUTES. DO NOT NEED TOKEN TO ACCESS
+// UNPROTECTED ROUTES
 app.use(express.static('public'));
 app.use('/auth', authRoutes);
 
-// Authentication middleware provided by express-jwt. 
 app.use(ejwt({
   secret: JWT_SECRET_KEY,
   getToken: function fromHeader (req) {
@@ -36,24 +32,22 @@ app.use(ejwt({
   },
 }));
 
-// PROTECTED FROM NOW ON. 
+// PROTECTED ROUTES 
 app.use(express.static('public-secure'));
 app.use('/breeds', breedRoutes);
 app.use('/api/favorites', favoritesRoutes);
 
 app.use('/', function(req, res, next) {
-  // So when someone goes to root it redirects to landing page
   res.sendFile(path.resolve('public/index.html'));
 });
 
-//If we're about to 401, redirect to the landing page
-//If user tries to signup with existing credentials, redirect to login
 app.use(function(err, req, res, next) {
   if(401 == err.status) {
     console.log(err);
     return res.redirect('/');
   }
   if (11000 == err.code) {
+    //11000 code means signup attempt with existing credentials
     console.log(err);
     return res.redirect('/auth/login');
   }
