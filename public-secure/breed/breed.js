c4a8a0f0b921a6c0bf15e2cc7c4cafd52cc986d1
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-04-02 16:27:59 -0700

    added buttons for showing and hiding divs. added float-grid styling to breed page

--- public-secure/breed/breed.js
+++ public-secure/breed/breed.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let image;
+
 function getDataFromAllApis(searchTerm) {
   $.get('/breeds/fetch/'+searchTerm)
     .done(function ({imageUrls, youTubeData, adoptionData}) {
@@ -15,16 +17,40 @@ function getDataFromAllApis(searchTerm) {
 
 function showAdoptionData(result) {
   let html = '';
-  for (let i=0; i<2; i++) {
-    html += `<div>
-              <p>Name: ${result[i].name['$t']}</p>
-              <img src=${result[i].media.photos.photo[0]['$t']} alt=${result[i].breeds.breed['$t']}>
-              <p>Contact Email: ${result[i].contact.email['$t']}</p>
-              <p>Contact Phone: ${result[i].contact.phone['$t']}</p>
-              <p>City: ${result[i].contact.city['$t']}</p>
-            </div>`;
+  for (let i=0; i<5; i++) {
+    if (result[i].media.photos.photo.length === 0) {
+      image = '<img class=\'adoption-image\' src=\'\' alt=\'No photos available\'>';
+    } else {
+      image = getAdoptionImage(result[i].media.photos.photo);
+    }
+    html += (
+      `<div class="row">
+        <div class="col-12">
+          <div class="col-4">
+            <h3>Name: ${result[i].name['$t']}</h3>
+            <p>Age: ${result[i].age['$t']}</p>
+          </div>
+          <div class="col-4 adoption-image-div">
+            ${image}
+          </div>
+          <div class="col-4">
+            <p>City: ${result[i].contact.city['$t']}</p>
+            <p>Contact Email: ${result[i].contact.email['$t']}</p>
+            <p>Contact Phone: ${result[i].contact.phone['$t']}</p>
+          </div>
+        </div>
+      </div>`);
   }
   $('.js-adoption-div').append(html);
+}
+
+function getAdoptionImage(array) {
+  for (let i=0; i<array.length; i++) {
+    if (array[i]['@size'] === 'x' || array[i]['@size'] === 'pn') {
+      return image = `<img class='adoption-image' src=${array[i]['$t']} alt='Adoption image'>`;
+    }
+  }
+  return image = '<img class=\'adoption-image\' src=\'\' alt=\'No photos available\'>';
 }
 
 function showDogImages(result) {
@@ -62,7 +88,25 @@ getDataFromAllApis(resultDog);
 //getting the breed name from the backend and using it to add a title
 //and make API calls
 
-$('.js-add-to-favorites-button').click(() => {
+$('.js-show-images').click(function() {
+  $('.js-images-div').removeAttr('hidden');
+  $('.js-videos-div').attr('hidden', true);
+  $('.js-adoption-div').attr('hidden', true);
+});
+
+$('.js-show-videos').click(function() {
+  $('.js-images-div').attr('hidden', true);
+  $('.js-videos-div').removeAttr('hidden');
+  $('.js-adoption-div').attr('hidden', true);
+});
+
+$('.js-show-adoptions').click(function() {
+  $('.js-images-div').attr('hidden', true);
+  $('.js-videos-div').attr('hidden', true);
+  $('.js-adoption-div').removeAttr('hidden');
+});
+
+$('.js-add-favorites').click(() => {
   $.ajax({
     url: '/api/favorites/' + resultDog,
     method: 'POST',
